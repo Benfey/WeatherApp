081c45e217aad418742cad5c7080a43f237f9ba7
Anton <anton.benfey@gmail.com>
2018-10-07 14:44:16 -0300

    Adding localized weather reports

--- app/src/main/java/com/example/anton/firstapp/MainActivity.java
+++ app/src/main/java/com/example/anton/firstapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.anton.firstapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
-import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.json.JSONArray;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -21,15 +34,46 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected Location mLastLocation;
+
+    Double latitude;
+    Double longitude;
+    FusedLocationProviderClient mFusedLocationClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Weather - Fredericton");
+        setTitle("Weather");
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
+
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+     private void getLastLocation() {
+        mFusedLocationClient.getLastLocation().addOnCompleteListener(this, new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful() && task.getResult() != null) {
+                    mLastLocation = task.getResult();
+                    latitude = mLastLocation.getLatitude();
+                    longitude = mLastLocation.getLongitude();
+                }
+            }
+        });
+     }
 
     public void getTemperature(View view){
         Button button = findViewById(R.id.change_button);
+
+        if (!checkPermissions()) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 34);
+            return;
+        }
+
         new LongRunningTask().execute();
         button.setEnabled(false);
         button.setAlpha(0.3f);
@@ -44,8 +88,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-                System.out.println("Sleeping...");
-                TimeUnit.SECONDS.sleep(5);
+                TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -68,14 +111,30 @@ public class MainActivity extends AppCompatActivity {
         TextView showCountTextView = findViewById(R.id.textView);
         String display = "";
         String inputLine;
+        Boolean error = false;
 
         @Override
         protected String doInBackground(Void... voids) {
+
             StringBuilder response = new StringBuilder();
+
+            if (latitude == null || longitude == null) {
+                getLastLocation();
+                while (latitude == null || longitude == null) {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            String url = "https://api.darksky.net/forecast/2be7c1d3f9b304be4a2a04ff1a84d576/" + latitude + "," + longitude;
+            System.out.println(url);
 
             try {
 
-                URL obj = new URL("https://api.darksky.net/forecast/2be7c1d3f9b304be4a2a04ff1a84d576/45.959856,-66.763270");
+                URL obj = new URL(url);
                 HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
                 con.setRequestMethod("GET");
@@ -96,17 +155,27 @@ public class MainActivity extends AppCompatActivity {
                 double tempF = currentWeather.getDouble("apparentTemperature");
                 int tempC = Math.round(((Math.round(tempF) - 32)*5)/9);
                 display = ("Feels like: " + String.valueOf(tempC) + "°");
+
             } catch (IOException | JSONException e) {
+
                 e.printStackTrace();
+                error = true;
+
             }
 
             return display;
+
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             showCountTextView.setText(display);
+            if (error) {
+                Toast myToast = Toast.makeText(getApplicationContext(), "Error fetching weather info. Is device connected to internet?", Toast.LENGTH_SHORT);
+                myToast.show();
+                error = false;
+            }
         }
     }
 }
